17a36fd604c119fa52e44f2c56fb39107761bf82
Anton Ber <berantonmsu@gmail.com>
2020-12-27 20:06:01 +0300

    add template page and updated requrements.txt

--- app/predict.py
+++ app/predict.py
@@ -1,15 +1,23 @@
 """Make genres prediction on given dialogue from movie"""
-from flask import Flask
-from classifier import Classifier
+
+import sys
+from flask import Flask, render_template, request
+
 from train import DEFAULT_MODEL_PATH, DEFAULT_VECTORIZER_PATH, DEFAULT_MLB_PATH
+from classifier import Classifier
+
+sys.path.append('./app/')
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+
+@app.route('/', methods=['POST', 'GET'])
+def index_page(text="", prediction_message=""):
     """Main page of web app"""
-    clf = Classifier(DEFAULT_MODEL_PATH,
-                     DEFAULT_VECTORIZER_PATH, DEFAULT_MLB_PATH)
-    dialogue = input()
-    prediction = clf.predict(dialogue)
-    print(" ".join(prediction))
+    if request.method == "POST":
+        clf = Classifier(DEFAULT_MODEL_PATH,
+                         DEFAULT_VECTORIZER_PATH, DEFAULT_MLB_PATH)
+        dialogue = request.form["text"]
+        prediction = clf.predict(dialogue)
+        prediction_message = " ".join(sorted(prediction))
+    return render_template('prediction_page.html', text=text, prediction_message=prediction_message)
