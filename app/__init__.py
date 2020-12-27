3fe97819f89119f519ba942bb35c57a149e467e5
Anton Ber <berantonmsu@gmail.com>
2020-12-27 21:54:04 +0300

    fixing bugs #8

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,1 @@
 from .predict import app
-from .preprocessing_utility import CleanTextPreprocessor, GenresListPreprocessor
-from .preprocessing import generate_preprocessed_train_X_y
-from .train import DEFAULT_MODEL_PATH, DEFAULT_VECTORIZER_PATH, DEFAULT_MLB_PATH
-from .classifier import Classifier
